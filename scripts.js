9a80bbf3a322f12f59fa40fc2a2dbd61f5db315b
Abdullah <abdullahayodeji005@gmail.com>
2024-12-06 22:15:16 +0100

    .

--- scripts.js
+++ scripts.js
@@ -2,3 +2,9 @@ let myDate = new Date();
 myDate.getFullYear;
 let dt = document.querySelector("#date");
 dt.innerText = myDate.getFullYear();
+
+let hamIcon = document.querySelector(".hmb-icon");
+let mobileNav = document.querySelector(".mobile-nav");
+hamIcon.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
